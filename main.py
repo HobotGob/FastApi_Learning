b11d69e2237e89fbf1d53e4a7b099aead27a0685
Hobot <andreiaeon@gmail.com>
2024-06-20 09:27:51 +0300

    Commit

--- main.py
+++ main.py
@@ -13,13 +13,23 @@ def home():
     #return {"message": "All Blogs found"}
 
 @app.get("/blog/all",
-         tags=["blog"]
+         tags=["blog"],
+         summary="Retrieve all blogs",
+         description="This api allows you to retrieve all blogs. "
          )
 def get_all_blogs(page = 1, page_size: Optional[int] = None):
     return {"message": f"All {page_size} blogs on page {page}"}
 
 @app.get("/blog/{id}/comments/{comment_id}", tags=["blog", "comment"])
 def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    """
+    Simulate retrieving comments of a blog
+
+    - **id** mandatory path parameter
+    - **comment_id** mandatory path parameter
+    - **valid** optional query parameter
+    - **username** optional query parameter
+    """
     return {"message": f"blog {id}, comment_id {comment_id}, valid {valid}, username {username}"}
 
 
@@ -31,6 +41,10 @@ class BlogType(StrEnum):
 
 @app.get("/blog/type/{type}", tags=["blog"])
 def get_blog_type(type: BlogType):
+    """
+    Simulate BlogType retrieval
+    - **type** Just you BlogType
+    """
     return {"message": f"Blog Type is {type}"}
 
 @app.get("/blog/{id}", status_code=status.HTTP_200_OK, tags=["blog"])
