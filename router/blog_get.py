2f88462dc3e28e08e5d8acb1c5b8df896e272d8a
Hobot <andreiaeon@gmail.com>
2024-06-29 09:56:25 +0300

    Commit

--- router/blog_get.py
+++ router/blog_get.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, status, Response
+from fastapi import APIRouter, status, Response, Depends
 from enum import StrEnum
 from typing import Optional
-
+from router.blog_post import requires_functionality
 router = APIRouter(
     prefix="/blog",
     tags=["blog"]
@@ -15,11 +15,11 @@ router = APIRouter(
          description="This api allows you to retrieve all blogs. ",
          response_description = "This list of available blogs"
          )
-def get_all_blogs(page = 1, page_size: Optional[int] = None):
-    return {"message": f"All {page_size} blogs on page {page}"}
+def get_all_blogs(page = 1, page_size: Optional[int] = None, req_parameter: dict = Depends(requires_functionality)):
+    return {"message": f"All {page_size} blogs on page {page}", "req": req_parameter}
 
 @router.get("/{id}/comments/{comment_id}", tags=["comment"])
-def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None, req_parameter: dict = Depends(requires_functionality)):
     """
     Simulate retrieving comments of a blog
 
@@ -38,7 +38,7 @@ class BlogType(StrEnum):
     howto = "howto"
 
 @router.get("/type/{type}")
-def get_blog_type(type: BlogType):
+def get_blog_type(type: BlogType, req_parameter: dict = Depends(requires_functionality)):
     """
     Simulate BlogType retrieval
 
@@ -47,7 +47,7 @@ def get_blog_type(type: BlogType):
     return {"message": f"Blog Type is {type}"}
 
 @router.get("/{id}", status_code=status.HTTP_200_OK)
-def get_blog(id: int, response: Response):
+def get_blog(id: int, response: Response, req_parameter: dict = Depends(requires_functionality)):
     if id > 5:
         response.status_code = status.HTTP_404_NOT_FOUND
         return {"error" f"Blog {id} not found"}
--- router/blog_post.py
+++ router/blog_post.py
@@ -1,17 +1,23 @@
-from fastapi import APIRouter, Query, Body
+from fastapi import APIRouter, Query, Body, Path
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List, Optional, Dict
 
 router = APIRouter(
     prefix="/blog",
     tags=["blog"]
 )
 
+class Image(BaseModel):
+    url: str
+    alias: str
 class BlogModel(BaseModel):
     title: str
     content: str
     nb_comments: int
     published: bool
+    tags: List[str] = []
+    metadata: Dict[str, str] = {"key: " "val1"}
+    image: Optional[Image] = None
 @router.post("/new{id}")
 def create_blog(blog: BlogModel, id: int, version: int = 1):
     return {
@@ -20,22 +26,26 @@ def create_blog(blog: BlogModel, id: int, version: int = 1):
         "version": version
         }
 
-@router.post("/new/{id}/comment")
-def create_comment(blog: BlogModel, id: int, comment_id: int = Query(None, title="id of the comment", description="Some description for comment",
-                                                                                  alias="commentId",
+@router.post("/new/{id}/comment/{comment_id}")
+def create_comment(blog: BlogModel, id: int, comment_title: int = Query(None, title="title of the comment", description="Some description for comment_title",
+                                                                                  alias="commentTitle",
                                                                                   deprecated=True),
                                                                                   content: str = Body(Ellipsis,
                                                                                                       min_length=10,
                                                                                                       max_length=50,
                                                                                                       regex="^[A-Za-z0-9_]+$"),
-                   v: Optional[List[str]] = Query(["1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1"])
-
+                   v: Optional[List[str]] = Query(["1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1"]),
+                   comment_id: Optional[int] = Path(gt=5, le=10)
                    ):
     return {
     "blog": blog,
     "id": id,
-    "comment_id": comment_id,
+    "comment_title": comment_title,
     "content": content,
-    "version": v
+    "version": v,
+    "comment_id": comment_id
 
 }
+
+def requires_functionality():
+    return {"message": "learning FastAPI is important"}
